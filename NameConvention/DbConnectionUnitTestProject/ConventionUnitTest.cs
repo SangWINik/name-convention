82d28eb7add6564fe54ce76a64f7c98d40512959
Maxym Holoborodko <go1oborodko97@gmail.com>
2017-03-21 19:27:50 +0200

    PrimaryKey Convention

--- NameConvention/DbConnectionUnitTestProject/ConventionUnitTest.cs
+++ NameConvention/DbConnectionUnitTestProject/ConventionUnitTest.cs
@@ -10,7 +10,7 @@ namespace DbConnectionUnitTestProject
         [TestMethod]
         public void ColumnTestMethod()
         {
-            Convention con = new Convention("conv", "", ":tName:_:MainPart:Column", "", false);
+            Convention con = new Convention("conv", "", ":TName:_:MainPart:_Column", "", false);
             Console.WriteLine(con.GetColumnName("Color", "Door"));
         }
 
@@ -19,6 +19,13 @@ namespace DbConnectionUnitTestProject
         {
             Convention con = new Convention("conv", ":MainPart:__++__:mainPart:", ":tName:_:MainPart:Column", "", true);
             Console.WriteLine(con.GetTableName("Doors"));
+        }
+
+        [TestMethod]
+        public void PrimaryKeyTestMethod()
+        {
+            Convention con = new Convention("conv", ":MainPart:__++__:mainPart:", ":tName:_:MainPart:Column", ":TName:_ID", true);
+            Console.WriteLine(con.GetPrimaryKeyName("ID", "Door"));
         }
     }
 }
--- NameConvention/NameConvention/db_features/Convention.cs
+++ NameConvention/NameConvention/db_features/Convention.cs
@@ -40,7 +40,7 @@ namespace NameConvention.db_features
             {
                 if (tName.Length > 0)
                     tName = tName.First().ToString().ToUpper() + tName.Substring(1);
-                result = result.Replace(":tName:", tName);
+                result = result.Replace(":TName:", tName);
             }
             if (result.IndexOf(":mainPart:") != -1)
             {
@@ -82,6 +82,38 @@ namespace NameConvention.db_features
             {
                 if (result[result.Length - 1] == 's')
                     result = result.Remove(result.Length - 1, 1);
+            }
+            return result;
+        }
+
+        public string GetPrimaryKeyName(string oldName, string tableName)
+        {
+            string tName = tableName;
+            string mainPart = oldName;
+            string result = PrimaryKeyTemplate;
+            if (result.IndexOf(":tName:") != -1)
+            {
+                if (tName.Length > 0)
+                    tName = tName.First().ToString().ToLower() + tName.Substring(1);
+                result = result.Replace(":tName:", tName);
+            }
+            if (result.IndexOf(":TName:") != -1)
+            {
+                if (tName.Length > 0)
+                    tName = tName.First().ToString().ToUpper() + tName.Substring(1);
+                result = result.Replace(":TName:", tName);
+            }
+            if (result.IndexOf(":mainPart:") != -1)
+            {
+                if (mainPart.Length > 0)
+                    mainPart = mainPart.First().ToString().ToLower() + mainPart.Substring(1);
+                result = result.Replace(":mainPart:", mainPart);
+            }
+            if (result.IndexOf(":MainPart:") != -1)
+            {
+                if (mainPart.Length > 0)
+                    mainPart = mainPart.First().ToString().ToUpper() + mainPart.Substring(1);
+                result = result.Replace(":MainPart:", mainPart);
             }
             return result;
         }
