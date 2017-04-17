303b78d5169fb395e52b472b4b22392e6a66b099
Maxym Holoborodko <go1oborodko97@gmail.com>
2017-04-18 00:13:21 +0300

    Added RenameColumnPK Method

--- NameConvention/NameConvention/db_features/Table.cs
+++ NameConvention/NameConvention/db_features/Table.cs
@@ -57,5 +57,22 @@ namespace NameConvention.db_features
             }
             catch (Exception ex) { }
         }
+
+        public void RenameColumnPK(string newName, SqlConnection conn)
+        {
+            string qs = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + QUOTENAME(CONSTRAINT_NAME)), 'IsPrimaryKey') = 1 AND TABLE_NAME = '" + _name + "'";
+            SqlCommand comm = new SqlCommand(qs, conn);
+            string colName = comm.ExecuteScalar().ToString();
+            //string QueryString = "ALTER TABLE " + _name + " RENAME COLUMN " + col.Name + " TO " + newName + ";";
+            string QueryString = "sp_rename '" + _name + "." + colName + "', '" + newName + "', 'COLUMN';";
+            SqlCommand command = new SqlCommand(QueryString, conn);
+            try
+            {
+                //conn.Open();
+                command.ExecuteNonQuery();
+                //conn.Close();
+            }
+            catch (Exception ex) { }
+        }
     }
 }
